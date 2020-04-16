bbe0117446999b8ac128dbacc8f83ce49353182d
shalu169 <shaliniagrawal16@gmail.com>
2020-04-16 14:25:56 +0530

    Numpy basics

---
+++ hackerrank_38.py
@@ -0,0 +1,25 @@
+# leap year
+
+def LeapYear(n):
+    if n%400 ==0 or (n%4==0 and n%100!=0):
+        print('True')
+    else:
+        print('False')
+
+n = int(input())
+LeapYear(n)
+
+# Adjecent character
+
+def FlipCharacter(s):
+    new_string = ''
+    for i in range()
+
+
+
+n=input()
+if len(n)%2 !=0:
+    print('Odd length')
+else:
+    FlipCharacter(n)
+
---
+++ longest_valid_paranthesis.py
@@ -0,0 +1,8 @@
+
+
+s = input()
+count = 0
+valid = 0
+for i in range(0, len(s)):
+    if s[i] == '(' and s[i+1] == ')'
+        count = count +1
